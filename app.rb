ecab77525ba2a6f11f72d58fae22934bedfee929
Ben Pickles <spideryoung@gmail.com>
2024-11-19 22:20:16 +0000

    Use Decant's new Content#relative_path to link to the page's Markdown on GitHub

--- app.rb
+++ app.rb
@@ -8,6 +8,10 @@ require_relative 'lib/kramdown_enhancements'
 # Define a frontmatter-aware content collection wrapper.
 Page = Decant.define(dir: 'content', ext: 'md') do
   frontmatter :title
+
+  def source_url
+    "https://github.com/benpickles/parklife.dev/blob/main/content/#{relative_path}"
+  end
 end
 
 # Configure Sinatra's Markdown rendering (Kramdown will be used because it's
@@ -21,5 +25,6 @@ get '/*slug' do
   slug = params[:slug].empty? ? 'home' : params[:slug]
   @page = Page.find(slug)
   @page_title = @page.title
+  @source_url = @page.source_url
   erb :page
 end
--- content/footer.md
+++
@@ -1,3 +0,0 @@
-Parklife is an [open source](https://github.com/benpickles/parklife) project from [Ben Pickles](https://www.benpickles.com/).
-
-This site is a Parklife/Sinatra app, it's also [open source](https://github.com/benpickles/parklife.dev).
